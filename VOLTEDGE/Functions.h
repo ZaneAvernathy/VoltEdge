94180c60b425bee645a07db285d5916fa0ea0727
ZaneAvernathy <zavernathy@gmail.com>
2022-12-05 04:24:35 -0500

    Added new `num` functions
    `logn`: logarithm of some value with some base.
    `bit_length`: get the number of bits needed to represent a number.
    `bit_count`: get the number of set bits in a number.
    `hex`: formatting helper to turn numbers into hex digit strings.

--- VOLTEDGE/Functions.h
+++ VOLTEDGE/Functions.h
@@ -101,6 +101,96 @@ GUARD_VOLTEDGE_FUNCTIONS :?= false
 
         min .sfunction A, B, (A <? B)
 
+      ; Created: 0.22
+      ; Updated: 0.22
+      ; logn(Value, Base)
+
+        ; Inputs:
+          ; Value: value to take the logarithm of.
+          ; Base: base to give the logarithm of.
+
+        ; Outputs:
+          ; logarithm of value to the base.
+
+        ; Gets the logarithm of a value to some base.
+
+        logn .sfunction Value, Base, (log(Value) / log(Base))
+
+      ; Created: 0.22
+      ; Updated: 0.22
+      ; bit_length(Value)
+
+        ; Inputs:
+          ; Value: value to count
+
+        ; Outputs:
+          ; number of bits
+
+        ; Counts the number of bits needed to represent an integer
+        ; in binary.
+
+        bit_length .function Value
+
+            Return := 0
+
+          .if (Value != 0)
+
+            Return := 1 + int(logn(abs(Value), 2))
+
+          .endif
+
+        .endfunction Return
+
+      ; Created: 0.22
+      ; Updated: 0.22
+      ; bit_count(Value)
+
+        ; Inputs:
+          ; Value: value to count
+
+        ; Outputs:
+          ; number of bits set
+
+        ; Gives the number of ones in the binary representation
+        ; of the absolute value of an integer.
+
+        bit_count .sfunction Value, zstr.count(str(bits(abs(Value))), "1")
+
+      ; Created: 0.22
+      ; Updated: 0.22
+      ; hex(Value, Width=?)
+
+        ; Inputs:
+          ; Value: number to format as a string
+          ; Width: number of bytes to express value as
+
+        ; Outputs:
+          ; String representing the number
+
+        ; Gives a string representation of a number in
+        ; hex. The optional `Width` parameter can be used
+        ; to override the padding of the output:
+
+        ; num.hex(42) -> "2A"
+        ; num.hex(42, 2) -> "002A"
+
+        ; Widths smaller than the minimum size needed
+        ; to express the number will be ignored.
+
+        hex .function Value, Width=?
+
+          ByteCount := (num.bit_length(Value) + 7) / 8
+
+          .if !(Width === ?)
+
+            ByteCount := Width < ByteCount ? ByteCount : Width
+
+          .endif ; !(Width === ?)
+
+          FormatString := format("%%0%dX", ByteCount * 2)
+
+        .endfunction format(FormatString, Value)
+
     .endnamespace ; num
 
     op .namespace
